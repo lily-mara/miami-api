3b350b90df45f000bdfcd0d5f7f04506e5381f0c
Nate Mara <natemara@gmail.com>
2014-04-06 19:29:04 -0400

    added function prototypes

---
+++ miami_api.py
@@ -0,0 +1,18 @@
+def get_open():
+	"""
+	returns: list of currently open dining locations
+	"""
+	pass
+
+def get_hours(location):
+	"""
+	returns: list of hours for given location on current weekday
+	"""
+	pass
+
+def get_today_hours():
+	"""
+	returns: JSON data containing hours of every open dining location on
+		current weekday
+	"""
+	pass
