b51ebe98af2e908e60f1ff35255e1df906c26005
Nate Mara <natemara@gmail.com>
2014-04-06 21:18:33 -0400

    wrote inside time range function

--- miami_api.py
+++ miami_api.py
@@ -1,8 +1,23 @@
 import json
+import time
 
 times = None
 with open('times.json', 'r') as json_file:
 	times = json.load(json_file)
+
+def main():
+	print(inside_time_range(2000, 2200))
+
+def inside_time_range(start, stop):
+	"""
+	Takes two ints, start and stop times in 24-h style time, returns True if it
+	is currently within that time range, false otherwise
+
+	returns: True or False if it is currently inside of the given time range
+	"""
+
+	now = int(time.strftime('%H%M'))
+	return start < now < stop
 
 def get_open():
 	"""
@@ -22,3 +37,6 @@ def get_today_hours():
 		current weekday
 	"""
 	pass
+
+if __name__ == '__main__':
+	main()
