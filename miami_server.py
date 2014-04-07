75b3ea4f94fc30b12ca7f38e7faef9a1f325dc05
Nate Mara <natemara@gmail.com>
2014-04-07 13:04:39 -0400

    added update url to server

--- miami_server.py
+++ miami_server.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-import tornado.escape
+import tornado.autoreload
 import tornado.ioloop
 import tornado.web
+import subprocess
 
 import miami_api
 
@@ -20,12 +21,21 @@ class TodayHoursHandler(tornado.web.RequestHandler):
 		response = miami_api.get_today_hours()
 		self.write(response)
 
+class UpdateHandler(tornado.web.RequestHandler):
+	def post(self):
+		subprocess.call(['git', 'pull', 'origin', 'master'])
+
 application = tornado.web.Application([
 	(r'/miami/open', OpenLocationHandler),
 	(r'/miami/hours/([a-zA-Z]+)', HoursHandler),
-	(r'/miami/today', TodayHoursHandler)
+	(r'/miami/today', TodayHoursHandler),
+	(r'/miami/upate', UpdateHandler)
 ])
 
 if __name__ == '__main__':
 	application.listen(5000)
-	tornado.ioloop.IOLoop.instance().start()
+	tornado.autoreload.watch('miami_server.py')
+
+	ioloop = tornado.ioloop.IOLoop.instance()
+	tornado.autoreload.start(ioloop)
+	ioloop.start()
