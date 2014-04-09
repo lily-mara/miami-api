2d80c93a42c2aa0444320ba28aba61caa514385d
Nate Mara <natemara@gmail.com>
2014-04-09 19:37:31 -0400

    locations with >2 hours open are colored

--- html_functions.py
+++ html_functions.py
@@ -13,14 +13,35 @@ def get_open_html():
 
 	for location in open_list:
 		to_close = get_time_to_close(location[1][1])
+		to_close_string = get_close_string(to_close)
 		close_time = format_time(location[1][1])
+		color = get_color(to_close)
+
 		return_list.append({
 			'name': location[0],
-			'to_close' : to_close,
-			'close_time': close_time
+			'to_close' : to_close_string,
+			'close_time': close_time,
+			'color': color
 			})
 
 	return return_list
+
+
+def get_color(time_to_close):
+	if time_to_close < 60 * 60:
+		return 'background:#cc9999'
+	if time_to_close < 60 * 60 * 2:
+		return 'background:#ffcc99'
+	return ''
+
+def rgb_to_hex(rgb):
+	return '#%02x%02x%02x' % rgb
+
+
+def get_close_string(time_to_close):
+	time_to_close = time.gmtime(time_to_close)
+	close_string = time.strftime('%Hh:%Mm', time_to_close)
+	return close_string
 
 
 def get_time_to_close(close):
@@ -32,10 +53,8 @@ def get_time_to_close(close):
 	close_epoch = time.mktime(close_time)
 	now_epoch = time.time()
 
-	difference = time.gmtime(close_epoch - now_epoch + 60)
-
-	difference_string = time.strftime('%Hh:%Mm', difference)
-	return difference_string
+	difference = close_epoch - now_epoch + 60
+	return difference
 
 
 def format_time(to_format):
