d6eb51f9049b6cb9bc7b3523b8c2fbb71e1bd847
Nate Mara <natemara@gmail.com>
2014-04-09 21:47:08 -0400

    added comments and changed get_color to get_style

--- html_functions.py
+++ html_functions.py
@@ -3,10 +3,23 @@ import time
 import miami_api
 
 def main():
-	print(get_open_html())
+	print(get_open_for_html())
 
 
 def get_open_for_html():
+	"""
+	Returns list of dicts holding information to be used by the dynamic
+	'index.html' page upon loading. Each dict in the list is formatted as
+	follows:
+
+	{
+		'name': name of dining location,
+		'to_close': time remaining until close formatted like '03h:12m'
+		'close_time': time of close formatted like '11:00 PM',
+		'style': the HTML style tag to use for these cells
+	}
+
+	"""
 	return_list = []
 	open_dict = miami_api.get_open()
 	open_list = list(open_dict.items())
@@ -15,36 +28,49 @@ def get_open_for_html():
 		to_close = get_time_to_close(location[1][1])
 		to_close_string = get_close_string(to_close)
 		close_time = format_time(location[1][1])
-		color = get_color(to_close)
+		style = get_style(to_close)
 
 		return_list.append({
 			'name': location[0],
 			'to_close' : to_close_string,
 			'close_time': close_time,
-			'color': color
+			'style': style
 		})
 
 	return return_list
 
 
-def get_color(time_to_close):
+def get_style(time_to_close):
+	"""
+	Takes the remaining open time for a location and returns an HTML style
+	string to be used on the row in the page. Currently, this only
+	includes background color.
+	"""
+
 	if time_to_close < 60 * 60:
+		# Red for times less than one hour
 		return 'background:#cc9999'
 	if time_to_close < 60 * 60 * 2:
+		# yellow for times less than two hours
 		return 'background:#ffcc99'
+	# green otherwise
 	return 'background:#99cc99'
-
-def rgb_to_hex(rgb):
-	return '#%02x%02x%02x' % rgb
 
 
 def get_close_string(time_to_close):
+	"""
+	Given a time in seconds, this will format it as a string like 03h:12m
+	"""
 	time_to_close = time.gmtime(time_to_close)
 	close_string = time.strftime('%Hh:%Mm', time_to_close)
 	return close_string
 
 
 def get_time_to_close(close):
+	"""
+	Returns the difference (in seconds) between the given parameter,
+	'close', and the current time.
+	"""
 	if type(close) == int:
 		close = str(close)
 	close += ' ' + str(time.strftime('%Y-%m-%d'))
@@ -58,6 +84,10 @@ def get_time_to_close(close):
 
 
 def format_time(to_format):
+	"""
+	Takes a string in the format 2200 and returns a string in the format
+	11:00 PM
+	"""
 	in_time = time.strptime(str(to_format), '%H%M')
 	return time.strftime('%I:%M %p', in_time)
 
